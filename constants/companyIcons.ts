a853f68bda3633cba135bedfb230d73b13f81e46
Nikita Kolosov <n.kolosov2003@mail.ru>
2026-08-27 04:54:36 -0700

    fix: use ReactElement instead of JSX.Element in CompanyIconEntry type

--- constants/companyIcons.ts
+++ constants/companyIcons.ts
@@ -1,10 +1,12 @@
+import { ReactElement } from "react"
+
 import { NKolosovIcon } from "@/components/icons"
 
 import { DEFAULT_COMPANY_ID } from "./company"
 
 export interface CompanyIconEntry {
   // SVG icon component that fills its wrapper via width/height="100%"
-  Icon: (props: { color: string }) => JSX.Element
+  Icon: (props: { color: string }) => ReactElement
   // Width-to-height ratio applied to the View sizing this icon
   aspectRatio: number
 }
